9740c8af0937c4a7bbd5e56e7933f69fa0238fa3
Natalie Arellano <narellano@vmware.com>
2021-03-23 16:35:35 -0400

    Update remote/remote.go
    Signed-off-by: Natalie Arellano <narellano@vmware.com>
    
    Co-authored-by: Yael Harel <43007598+yaelharel@users.noreply.github.com>

--- remote/remote.go
+++ remote/remote.go
@@ -189,7 +189,7 @@ func newV1Image(keychain authn.Keychain, repoName string, platform imgutil.Platf
 	}
 
 	var image v1.Image
-	for i := 0; i < maxRetries+1; i++ {
+	for i := 0; i <= maxRetries; i++ {
 		time.Sleep(100 * time.Duration(i) * time.Millisecond) // wait if retrying
 		image, err = remote.Image(ref, remote.WithAuth(auth), remote.WithTransport(http.DefaultTransport), remote.WithPlatform(v1Platform))
 		if err != nil {
