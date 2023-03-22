9e5c0113733d89ee181e2665c69200c95d7b3e2f
Natalie Arellano <narellano@vmware.com>
2023-03-22 10:33:00 -0400

    Re-order remote implementation to match interface
    Signed-off-by: Natalie Arellano <narellano@vmware.com>

--- remote/remote.go
+++ remote/remote.go
@@ -37,6 +37,112 @@ type registrySetting struct {
 	insecureSkipVerify bool
 }
 
+// getters
+
+func (i *Image) Architecture() (string, error) {
+	cfg, err := i.image.ConfigFile()
+	if err != nil {
+		return "", errors.Wrapf(err, "getting config file for image %q", i.repoName)
+	}
+	if cfg == nil {
+		return "", fmt.Errorf("missing config for image %q", i.repoName)
+	}
+	if cfg.Architecture == "" {
+		return "", fmt.Errorf("missing Architecture for image %q", i.repoName)
+	}
+	return cfg.Architecture, nil
+}
+
+func (i *Image) CreatedAt() (time.Time, error) {
+	configFile, err := i.image.ConfigFile()
+	if err != nil {
+		return time.Time{}, errors.Wrapf(err, "getting createdAt time for image %q", i.repoName)
+	}
+	return configFile.Created.UTC(), nil
+}
+
+func (i *Image) Entrypoint() ([]string, error) {
+	cfg, err := i.image.ConfigFile()
+	if err != nil {
+		return nil, errors.Wrapf(err, "getting config file for image %q", i.repoName)
+	}
+	if cfg == nil {
+		return nil, fmt.Errorf("missing config for image %q", i.repoName)
+	}
+	return cfg.Config.Entrypoint, nil
+}
+
+func (i *Image) Env(key string) (string, error) {
+	cfg, err := i.image.ConfigFile()
+	if err != nil {
+		return "", errors.Wrapf(err, "getting config file for image %q", i.repoName)
+	}
+	if cfg == nil {
+		return "", fmt.Errorf("missing config for image %q", i.repoName)
+	}
+	for _, envVar := range cfg.Config.Env {
+		parts := strings.Split(envVar, "=")
+		if parts[0] == key {
+			return parts[1], nil
+		}
+	}
+	return "", nil
+}
+
+func (i *Image) Found() bool {
+	_, err := i.found()
+	return err == nil
+}
+
+func (i *Image) found() (*v1.Descriptor, error) {
+	reg := getRegistry(i.repoName, i.registrySettings)
+	ref, auth, err := referenceForRepoName(i.keychain, i.repoName, reg.insecure)
+	if err != nil {
+		return nil, err
+	}
+	return remote.Head(ref, remote.WithAuth(auth), remote.WithTransport(http.DefaultTransport))
+}
+
+func (i *Image) GetAnnotateRefName() (string, error) {
+	// TODO issue https://github.com/buildpacks/imgutil/issues/178
+	return "", errors.New("not yet implemented")
+}
+
+func (i *Image) GetLayer(sha string) (io.ReadCloser, error) {
+	layers, err := i.image.Layers()
+	if err != nil {
+		return nil, err
+	}
+
+	layer, err := findLayerWithSha(layers, sha)
+	if err != nil {
+		return nil, err
+	}
+
+	return layer.Uncompressed()
+}
+
+func (i *Image) Identifier() (imgutil.Identifier, error) {
+	ref, err := name.ParseReference(i.repoName, name.WeakValidation)
+	if err != nil {
+		return nil, errors.Wrapf(err, "parsing reference for image %q", i.repoName)
+	}
+
+	hash, err := i.image.Digest()
+	if err != nil {
+		return nil, errors.Wrapf(err, "getting digest for image %q", i.repoName)
+	}
+
+	digestRef, err := name.NewDigest(fmt.Sprintf("%s@%s", ref.Context().Name(), hash.String()), name.WeakValidation)
+	if err != nil {
+		return nil, errors.Wrap(err, "creating digest reference")
+	}
+
+	return DigestIdentifier{
+		Digest: digestRef,
+	}, nil
+}
+
 func (i *Image) Label(key string) (string, error) {
 	cfg, err := i.image.ConfigFile()
 	if err != nil {
@@ -60,43 +166,12 @@ func (i *Image) Labels() (map[string]string, error) {
 	return cfg.Config.Labels, nil
 }
 
-func (i *Image) Env(key string) (string, error) {
-	cfg, err := i.image.ConfigFile()
-	if err != nil {
-		return "", errors.Wrapf(err, "getting config file for image %q", i.repoName)
-	}
-	if cfg == nil {
-		return "", fmt.Errorf("missing config for image %q", i.repoName)
-	}
-	for _, envVar := range cfg.Config.Env {
-		parts := strings.Split(envVar, "=")
-		if parts[0] == key {
-			return parts[1], nil
-		}
-	}
-	return "", nil
+func (i *Image) ManifestSize() (int64, error) {
+	return i.image.Size()
 }
 
-func (i *Image) WorkingDir() (string, error) {
-	cfg, err := i.image.ConfigFile()
-	if err != nil {
-		return "", errors.Wrapf(err, "getting config file for image %q", i.repoName)
-	}
-	if cfg == nil {
-		return "", fmt.Errorf("missing config for image %q", i.repoName)
-	}
-	return cfg.Config.WorkingDir, nil
-}
-
-func (i *Image) Entrypoint() ([]string, error) {
-	cfg, err := i.image.ConfigFile()
-	if err != nil {
-		return nil, errors.Wrapf(err, "getting config file for image %q", i.repoName)
-	}
-	if cfg == nil {
-		return nil, fmt.Errorf("missing config for image %q", i.repoName)
-	}
-	return cfg.Config.Entrypoint, nil
+func (i *Image) Name() string {
+	return i.repoName
 }
 
 func (i *Image) OS() (string, error) {
@@ -124,18 +199,20 @@ func (i *Image) OSVersion() (string, error) {
 	return cfg.OSVersion, nil
 }
 
-func (i *Image) Architecture() (string, error) {
-	cfg, err := i.image.ConfigFile()
+func (i *Image) TopLayer() (string, error) {
+	all, err := i.image.Layers()
 	if err != nil {
-		return "", errors.Wrapf(err, "getting config file for image %q", i.repoName)
+		return "", err
 	}
-	if cfg == nil {
-		return "", fmt.Errorf("missing config for image %q", i.repoName)
+	if len(all) == 0 {
+		return "", fmt.Errorf("image %q has no layers", i.Name())
 	}
-	if cfg.Architecture == "" {
-		return "", fmt.Errorf("missing Architecture for image %q", i.repoName)
+	topLayer := all[len(all)-1]
+	hex, err := topLayer.DiffID()
+	if err != nil {
+		return "", err
 	}
-	return cfg.Architecture, nil
+	return hex.String(), nil
 }
 
 func (i *Image) Variant() (string, error) {
@@ -149,76 +226,172 @@ func (i *Image) Variant() (string, error) {
 	return cfg.Variant, nil // it's optional so we don't care whether it's ""
 }
 
+func (i *Image) WorkingDir() (string, error) {
+	cfg, err := i.image.ConfigFile()
+	if err != nil {
+		return "", errors.Wrapf(err, "getting config file for image %q", i.repoName)
+	}
+	if cfg == nil {
+		return "", fmt.Errorf("missing config for image %q", i.repoName)
+	}
+	return cfg.Config.WorkingDir, nil
+}
+
+// setters
+
+func (i *Image) AnnotateRefName(refName string) error {
+	// TODO issue https://github.com/buildpacks/imgutil/issues/178
+	return errors.New("not yet implemented")
+}
+
 func (i *Image) Rename(name string) {
 	i.repoName = name
 }
 
-func (i *Image) Name() string {
-	return i.repoName
-}
-
-func (i *Image) Found() bool {
-	_, err := i.found()
-	return err == nil
-}
-
-func (i *Image) CheckReadWriteAccess() bool {
-	reg := getRegistry(i.repoName, i.registrySettings)
-	ref, _, err := referenceForRepoName(i.keychain, i.repoName, reg.insecure)
-	if err != nil {
-		return false
-	}
-	return i.CheckReadAccess() && remote.CheckPushPermission(ref, i.keychain, http.DefaultTransport) == nil
-}
-
-func (i *Image) CheckReadAccess() bool {
-	_, err := i.found()
-	if err != nil {
-		if transportErr, ok := err.(*transport.Error); ok {
-			return transportErr.StatusCode != http.StatusUnauthorized &&
-				transportErr.StatusCode != http.StatusForbidden
-		}
-		return false
-	}
-	return true
-}
-
-func (i *Image) found() (*v1.Descriptor, error) {
-	reg := getRegistry(i.repoName, i.registrySettings)
-	ref, auth, err := referenceForRepoName(i.keychain, i.repoName, reg.insecure)
-	if err != nil {
-		return nil, err
-	}
-	return remote.Head(ref, remote.WithAuth(auth), remote.WithTransport(http.DefaultTransport))
-}
-
-func (i *Image) Identifier() (imgutil.Identifier, error) {
-	ref, err := name.ParseReference(i.repoName, name.WeakValidation)
-	if err != nil {
-		return nil, errors.Wrapf(err, "parsing reference for image %q", i.repoName)
-	}
-
-	hash, err := i.image.Digest()
-	if err != nil {
-		return nil, errors.Wrapf(err, "getting digest for image %q", i.repoName)
-	}
-
-	digestRef, err := name.NewDigest(fmt.Sprintf("%s@%s", ref.Context().Name(), hash.String()), name.WeakValidation)
-	if err != nil {
-		return nil, errors.Wrap(err, "creating digest reference")
-	}
-
-	return DigestIdentifier{
-		Digest: digestRef,
-	}, nil
-}
-
-func (i *Image) CreatedAt() (time.Time, error) {
+func (i *Image) SetArchitecture(architecture string) error {
 	configFile, err := i.image.ConfigFile()
 	if err != nil {
-		return time.Time{}, errors.Wrapf(err, "getting createdAt time for image %q", i.repoName)
+		return err
 	}
-	return configFile.Created.UTC(), nil
+	configFile.Architecture = architecture
+	i.image, err = mutate.ConfigFile(i.image, configFile)
+	return err
+}
+
+func (i *Image) SetCmd(cmd ...string) error {
+	configFile, err := i.image.ConfigFile()
+	if err != nil {
+		return err
+	}
+	config := *configFile.Config.DeepCopy()
+	config.Cmd = cmd
+	i.image, err = mutate.Config(i.image, config)
+	return err
+}
+
+func (i *Image) SetEntrypoint(ep ...string) error {
+	configFile, err := i.image.ConfigFile()
+	if err != nil {
+		return err
+	}
+	config := *configFile.Config.DeepCopy()
+	config.Entrypoint = ep
+	i.image, err = mutate.Config(i.image, config)
+	return err
+}
+
+func (i *Image) SetEnv(key, val string) error {
+	configFile, err := i.image.ConfigFile()
+	if err != nil {
+		return err
+	}
+	config := *configFile.Config.DeepCopy()
+	ignoreCase := configFile.OS == "windows"
+	for idx, e := range config.Env {
+		parts := strings.Split(e, "=")
+		foundKey := parts[0]
+		searchKey := key
+		if ignoreCase {
+			foundKey = strings.ToUpper(foundKey)
+			searchKey = strings.ToUpper(searchKey)
+		}
+		if foundKey == searchKey {
+			config.Env[idx] = fmt.Sprintf("%s=%s", key, val)
+			i.image, err = mutate.Config(i.image, config)
+			return err
+		}
+	}
+	config.Env = append(config.Env, fmt.Sprintf("%s=%s", key, val))
+	i.image, err = mutate.Config(i.image, config)
+	return err
+}
+
+func (i *Image) SetLabel(key, val string) error {
+	configFile, err := i.image.ConfigFile()
+	if err != nil {
+		return err
+	}
+	config := *configFile.Config.DeepCopy()
+	if config.Labels == nil {
+		config.Labels = map[string]string{}
+	}
+	config.Labels[key] = val
+	i.image, err = mutate.Config(i.image, config)
+	return err
+}
+
+func (i *Image) SetOS(osVal string) error {
+	configFile, err := i.image.ConfigFile()
+	if err != nil {
+		return err
+	}
+	configFile.OS = osVal
+	i.image, err = mutate.ConfigFile(i.image, configFile)
+	return err
+}
+
+func (i *Image) SetOSVersion(osVersion string) error {
+	configFile, err := i.image.ConfigFile()
+	if err != nil {
+		return err
+	}
+	configFile.OSVersion = osVersion
+	i.image, err = mutate.ConfigFile(i.image, configFile)
+	return err
+}
+
+func (i *Image) SetVariant(variant string) error {
+	configFile, err := i.image.ConfigFile()
+	if err != nil {
+		return err
+	}
+	configFile.Variant = variant
+	i.image, err = mutate.ConfigFile(i.image, configFile)
+	return err
+}
+
+func (i *Image) SetWorkingDir(dir string) error {
+	configFile, err := i.image.ConfigFile()
+	if err != nil {
+		return err
+	}
+	config := *configFile.Config.DeepCopy()
+	config.WorkingDir = dir
+	i.image, err = mutate.Config(i.image, config)
+	return err
+}
+
+// modifiers
+
+func (i *Image) AddLayer(path string) error {
+	layer, err := tarball.LayerFromFile(path)
+	if err != nil {
+		return err
+	}
+	i.image, err = mutate.AppendLayers(i.image, layer)
+	if err != nil {
+		return errors.Wrap(err, "add layer")
+	}
+	return nil
+}
+
+func (i *Image) AddLayerWithDiffID(path, diffID string) error {
+	// this is equivalent to AddLayer in the remote case
+	// it exists to provide optimize performance for local images
+	return i.AddLayer(path)
+}
+
+func (i *Image) Delete() error {
+	id, err := i.Identifier()
+	if err != nil {
+		return err
+	}
+	reg := getRegistry(i.repoName, i.registrySettings)
+	ref, auth, err := referenceForRepoName(i.keychain, id.String(), reg.insecure)
+	if err != nil {
+		return err
+	}
+	return remote.Delete(ref, remote.WithAuth(auth))
 }
 
 func (i *Image) Rebase(baseTopLayer string, newBase imgutil.Image) error {
@@ -255,20 +428,6 @@ func (i *Image) Rebase(baseTopLayer string, newBase imgutil.Image) error {
 	return nil
 }
 
-func (i *Image) SetLabel(key, val string) error {
-	configFile, err := i.image.ConfigFile()
-	if err != nil {
-		return err
-	}
-	config := *configFile.Config.DeepCopy()
-	if config.Labels == nil {
-		config.Labels = map[string]string{}
-	}
-	config.Labels[key] = val
-	i.image, err = mutate.Config(i.image, config)
-	return err
-}
-
 func (i *Image) RemoveLabel(key string) error {
 	cfg, err := i.image.ConfigFile()
 	if err != nil {
@@ -283,153 +442,6 @@ func (i *Image) RemoveLabel(key string) error {
 	return err
 }
 
-func (i *Image) SetEnv(key, val string) error {
-	configFile, err := i.image.ConfigFile()
-	if err != nil {
-		return err
-	}
-	config := *configFile.Config.DeepCopy()
-	ignoreCase := configFile.OS == "windows"
-	for idx, e := range config.Env {
-		parts := strings.Split(e, "=")
-		foundKey := parts[0]
-		searchKey := key
-		if ignoreCase {
-			foundKey = strings.ToUpper(foundKey)
-			searchKey = strings.ToUpper(searchKey)
-		}
-		if foundKey == searchKey {
-			config.Env[idx] = fmt.Sprintf("%s=%s", key, val)
-			i.image, err = mutate.Config(i.image, config)
-			return err
-		}
-	}
-	config.Env = append(config.Env, fmt.Sprintf("%s=%s", key, val))
-	i.image, err = mutate.Config(i.image, config)
-	return err
-}
-
-func (i *Image) SetWorkingDir(dir string) error {
-	configFile, err := i.image.ConfigFile()
-	if err != nil {
-		return err
-	}
-	config := *configFile.Config.DeepCopy()
-	config.WorkingDir = dir
-	i.image, err = mutate.Config(i.image, config)
-	return err
-}
-
-func (i *Image) SetEntrypoint(ep ...string) error {
-	configFile, err := i.image.ConfigFile()
-	if err != nil {
-		return err
-	}
-	config := *configFile.Config.DeepCopy()
-	config.Entrypoint = ep
-	i.image, err = mutate.Config(i.image, config)
-	return err
-}
-
-func (i *Image) SetCmd(cmd ...string) error {
-	configFile, err := i.image.ConfigFile()
-	if err != nil {
-		return err
-	}
-	config := *configFile.Config.DeepCopy()
-	config.Cmd = cmd
-	i.image, err = mutate.Config(i.image, config)
-	return err
-}
-
-func (i *Image) SetOS(osVal string) error {
-	configFile, err := i.image.ConfigFile()
-	if err != nil {
-		return err
-	}
-	configFile.OS = osVal
-	i.image, err = mutate.ConfigFile(i.image, configFile)
-	return err
-}
-
-func (i *Image) SetOSVersion(osVersion string) error {
-	configFile, err := i.image.ConfigFile()
-	if err != nil {
-		return err
-	}
-	configFile.OSVersion = osVersion
-	i.image, err = mutate.ConfigFile(i.image, configFile)
-	return err
-}
-
-func (i *Image) SetArchitecture(architecture string) error {
-	configFile, err := i.image.ConfigFile()
-	if err != nil {
-		return err
-	}
-	configFile.Architecture = architecture
-	i.image, err = mutate.ConfigFile(i.image, configFile)
-	return err
-}
-
-func (i *Image) SetVariant(variant string) error {
-	configFile, err := i.image.ConfigFile()
-	if err != nil {
-		return err
-	}
-	configFile.Variant = variant
-	i.image, err = mutate.ConfigFile(i.image, configFile)
-	return err
-}
-
-func (i *Image) TopLayer() (string, error) {
-	all, err := i.image.Layers()
-	if err != nil {
-		return "", err
-	}
-	if len(all) == 0 {
-		return "", fmt.Errorf("image %q has no layers", i.Name())
-	}
-	topLayer := all[len(all)-1]
-	hex, err := topLayer.DiffID()
-	if err != nil {
-		return "", err
-	}
-	return hex.String(), nil
-}
-
-func (i *Image) GetLayer(sha string) (io.ReadCloser, error) {
-	layers, err := i.image.Layers()
-	if err != nil {
-		return nil, err
-	}
-
-	layer, err := findLayerWithSha(layers, sha)
-	if err != nil {
-		return nil, err
-	}
-
-	return layer.Uncompressed()
-}
-
-func (i *Image) AddLayer(path string) error {
-	layer, err := tarball.LayerFromFile(path)
-	if err != nil {
-		return err
-	}
-	i.image, err = mutate.AppendLayers(i.image, layer)
-	if err != nil {
-		return errors.Wrap(err, "add layer")
-	}
-	return nil
-}
-
-func (i *Image) AddLayerWithDiffID(path, diffID string) error {
-	// this is equivalent to AddLayer in the remote case
-	// it exists to provide optimize performance for local images
-	return i.AddLayer(path)
-}
-
 func (i *Image) ReuseLayer(sha string) error {
 	layer, err := findLayerWithSha(i.prevLayers, sha)
 	if err != nil {
@@ -438,6 +450,31 @@ func (i *Image) ReuseLayer(sha string) error {
 	i.image, err = mutate.AppendLayers(i.image, layer)
 	return err
 }
+
+// extras
+
+func (i *Image) CheckReadAccess() bool {
+	_, err := i.found()
+	if err != nil {
+		if transportErr, ok := err.(*transport.Error); ok {
+			return transportErr.StatusCode != http.StatusUnauthorized &&
+				transportErr.StatusCode != http.StatusForbidden
+		}
+		return false
+	}
+	return true
+}
+
+func (i *Image) CheckReadWriteAccess() bool {
+	reg := getRegistry(i.repoName, i.registrySettings)
+	ref, _, err := referenceForRepoName(i.keychain, i.repoName, reg.insecure)
+	if err != nil {
+		return false
+	}
+	return i.CheckReadAccess() && remote.CheckPushPermission(ref, i.keychain, http.DefaultTransport) == nil
+}
+
+// helpers
 
 func findLayerWithSha(layers []v1.Layer, diffID string) (v1.Layer, error) {
 	for _, layer := range layers {
@@ -452,32 +489,7 @@ func findLayerWithSha(layers []v1.Layer, diffID string) (v1.Layer, error) {
 	return nil, fmt.Errorf("previous image did not have layer with diff id %q", diffID)
 }
 
-func (i *Image) Delete() error {
-	id, err := i.Identifier()
-	if err != nil {
-		return err
-	}
-	reg := getRegistry(i.repoName, i.registrySettings)
-	ref, auth, err := referenceForRepoName(i.keychain, id.String(), reg.insecure)
-	if err != nil {
-		return err
-	}
-	return remote.Delete(ref, remote.WithAuth(auth))
-}
-
-func (i *Image) ManifestSize() (int64, error) {
-	return i.image.Size()
-}
-
-func (i *Image) AnnotateRefName(refName string) error {
-	// TODO issue https://github.com/buildpacks/imgutil/issues/178
-	return errors.New("not yet implemented")
-}
-
-func (i *Image) GetAnnotateRefName() (string, error) {
-	// TODO issue https://github.com/buildpacks/imgutil/issues/178
-	return "", errors.New("not yet implemented")
-}
+// for rebase
 
 type subImage struct {
 	img       v1.Image
