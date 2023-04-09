db82b6956228df34b4276951bf13c127980cd74d
Husni Faiz <ahamedhusni73@gmail.com>
2023-04-09 11:36:57 +0530

    index: add image index types
    Signed-off-by: Husni Faiz <ahamedhusni73@gmail.com>

--- index.go
+++ index.go
@@ -1,7 +1,20 @@
 package imgutil
 
+import "github.com/google/go-containerregistry/pkg/v1/types"
+
 type ImageIndex interface {
 	Add(repoName string) error
 	Remove(repoName string) error
 	Save() error
+}
+
+func (t MediaTypes) IndexManifestType() types.MediaType {
+	switch t {
+	case OCITypes:
+		return types.OCIImageIndex
+	case DockerTypes:
+		return types.DockerManifestList
+	default:
+		return ""
+	}
 }
