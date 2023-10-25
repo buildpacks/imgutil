67824e989715b0bdd3793f4df3d2e55d643052d8
Natalie Arellano <narellano@vmware.com>
2023-10-25 14:07:23 -0400

    Fix local images when daemon uses containerd storage
    Signed-off-by: Natalie Arellano <narellano@vmware.com>

--- acceptance/reproducibility_test.go
+++ acceptance/reproducibility_test.go
@@ -164,6 +164,12 @@ func compare(t *testing.T, img1, img2 string) {
 	cfg2, err := v1img2.ConfigFile()
 	h.AssertNil(t, err)
 
+	// images that were created locally may have `DockerVersion` equal to "dev" and missing `Config.Image`
+	cfg1.DockerVersion = ""
+	cfg2.DockerVersion = ""
+	cfg1.Config.Image = ""
+	cfg2.Config.Image = ""
+
 	h.AssertEq(t, cfg1, cfg2)
 
 	h.AssertEq(t, ref1.Identifier(), ref2.Identifier())
