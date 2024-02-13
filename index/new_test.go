1d563fda16d2cbc818715db6fe61d858d98f42d2
WYGIN <wygininc@gmail.com>
2024-02-13 10:39:54 +0000

    WIP added tests for index.NewIndex
    Signed-off-by: WYGIN <wygininc@gmail.com>

--- index/new_test.go
+++ index/new_test.go
@@ -1,11 +1,16 @@
 package index_test
 
 import (
+	"os"
 	"testing"
 
+	"github.com/google/go-containerregistry/pkg/v1/types"
 	"github.com/sclevine/spec"
 	"github.com/sclevine/spec/report"
-	// h "github.com/buildpacks/imgutil/testhelpers"
+
+	"github.com/buildpacks/imgutil"
+	"github.com/buildpacks/imgutil/index"
+	h "github.com/buildpacks/imgutil/testhelpers"
 )
 
 func TestRemoteNew(t *testing.T) {
@@ -14,11 +19,48 @@ func TestRemoteNew(t *testing.T) {
 
 func testRemoteNew(t *testing.T, when spec.G, it spec.S) {
 	when("#NewIndex", func() {
-		it("should have expected indexOptions", func() {})
-		it("should return an error when invalid repoName is passed", func() {})
-		it("should return an error when index with the given repoName doesn't exists", func() {})
-		it("should return ImageIndex with expected output", func() {})
-		it("should able to call #ImageIndex", func() {})
-		it("should able to call #Image", func() {})
+		var (
+			idx     imgutil.ImageIndex
+			err     error
+			xdgPath = "xdgPath"
+		)
+		it.After(func() {
+			h.AssertNil(t, os.RemoveAll(xdgPath))
+		})
+		it("should have expected indexOptions", func() {
+			idx, err = index.NewIndex("repo/name", index.WithInsecure(true), index.WithXDGRuntimePath(xdgPath))
+			h.AssertNil(t, err)
+			h.AssertEq(t, idx.(*imgutil.IndexHandler).Options.InsecureRegistry, true)
+		})
+		it("should return an error when invalid repoName is passed", func() {
+			idx, err = index.NewIndex("invalid/repoName", index.WithInsecure(true), index.WithXDGRuntimePath(xdgPath))
+			h.AssertNotEq(t, err, nil)
+		})
+		it("should return ManifestHanler", func() {
+			idx, err = index.NewIndex("repo/name", index.WithInsecure(true), index.WithManifestOnly(true), index.WithXDGRuntimePath(xdgPath))
+			h.AssertNil(t, err)
+
+			_, ok := idx.(*imgutil.ManifestHandler)
+			h.AssertEq(t, ok, true)
+		})
+		it("should return IndexHandler", func() {
+			idx, err = index.NewIndex("repo/name", index.WithInsecure(true), index.WithXDGRuntimePath(xdgPath))
+			h.AssertNil(t, err)
+
+			_, ok := idx.(*imgutil.IndexHandler)
+			h.AssertEq(t, ok, true)
+		})
+		it("should return ImageIndex with expected format", func() {
+			idx, err := index.NewIndex("repo/name", index.WithFormat(types.DockerManifestList), index.WithXDGRuntimePath(xdgPath))
+			h.AssertNil(t, err)
+
+			imgIdx, ok := idx.(*imgutil.IndexHandler)
+			h.AssertEq(t, ok, true)
+
+			mfest, err := imgIdx.IndexManifest()
+			h.AssertNil(t, err)
+			h.AssertNotEq(t, mfest, nil)
+			h.AssertEq(t, mfest.MediaType, types.DockerManifestList)
+		})
 	})
 }
