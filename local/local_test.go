db66ae4d5cf72a9523cae7184c8cf6530827e4bb
Natalie Arellano <narellano@vmware.com>
2024-04-12 10:36:54 -0400

    Add and fix test
    Signed-off-by: Natalie Arellano <narellano@vmware.com>

--- local/local_test.go
+++ local/local_test.go
@@ -1584,11 +1584,9 @@ func testImage(t *testing.T, when spec.G, it spec.S) {
 					h.AssertNil(t, err)
 					h.AssertNil(t, err)
 					_, err = img.GetLayer(someSHA)
-					h.AssertError(
-						t,
-						err,
-						fmt.Sprintf(`image %q does not contain layer with diff ID "%s"`, repoName, someSHA),
-					)
+					h.AssertError(t, err, fmt.Sprintf("failed to find layer with diff ID %q", someSHA))
+					_, ok := err.(imgutil.ErrLayerNotFound)
+					h.AssertEq(t, ok, true)
 				})
 			})
 		})
@@ -1600,7 +1598,9 @@ func testImage(t *testing.T, when spec.G, it spec.S) {
 
 				readCloser, err := image.GetLayer(someSHA)
 				h.AssertNil(t, readCloser)
-				h.AssertError(t, err, fmt.Sprintf("image %q does not contain layer with diff ID %q", "not-exist", someSHA))
+				h.AssertError(t, err, fmt.Sprintf("failed to find layer with diff ID %q", someSHA))
+				_, ok := err.(imgutil.ErrLayerNotFound)
+				h.AssertEq(t, ok, true)
 			})
 		})
 	})
